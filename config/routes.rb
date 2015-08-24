88d0ae71f00f7a41863e42615e0d34d6a7168c62
sinansahin <bote.sinansahin@gmail.com>
2015-08-24 16:08:03 +0300

    navbar düzenlendi. home controller eklendi

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :news
   resources :activities
   resources :clubs
+  get 'home/yonerge' => 'home#yonerge'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
