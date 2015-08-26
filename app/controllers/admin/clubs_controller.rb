4f8115fdd2911ed3a397545d5175145c431071ca
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-08-26 14:44:36 +0300

    Admin Panel ve kulupler duzenlenmesi sağlandi

--- app/controllers/admin/clubs_controller.rb
+++ app/controllers/admin/clubs_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ClubsController < ApplicationController
+  layout 'admin'
   before_action :set_club, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:edit, :update, :new, :destroy, :create]
   
