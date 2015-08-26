60ba85d9fb884d1cda4a8379a3a4e3426448343b
Ecmel Kaytazoğlu <ecmel.kytz@gmail.com>
2015-08-26 09:38:38 +0300

    User Role modelleri için join table oluştur. #8

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,14 @@ ActiveRecord::Schema.define(version: 20150825182603) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "role_id", null: false
+  end
+
+  add_index "roles_users", ["role_id"], name: "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], name: "index_roles_users_on_user_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -82,7 +90,6 @@ ActiveRecord::Schema.define(version: 20150825182603) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "student_number"
-    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
