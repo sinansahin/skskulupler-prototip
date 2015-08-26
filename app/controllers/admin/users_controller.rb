9694deb8522df8e75b9bf3f700077a066f4ccd9b
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-08-26 16:44:28 +0300

    ana sayfaya yönetici paneline erişim sağlandi bazi formlar düzenlendi

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:edit, :update, :new, :destroy, :create]
 
+
   
   # GET /users
   # GET /users.json
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
+	include Pundit
+  protect_from_forgery
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  include Pundit
-  protect_from_forgery
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  respond_to :json, :html
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name) }
+  end
 
 end
