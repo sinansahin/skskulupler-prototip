59df4c0bc78ac6af03651b518ee203399b0eda0c
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-08-27 14:51:29 +0300

    info policy eklendi

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :first_name, :last_name, :email, presence: true
-- validates :password, presence: true, on: :create
+  validates :password, presence: true, on: :create
   validates :first_name, :last_name, length: {in: 3..50}
   #before_create :set_default_role
   # def set_default_role
