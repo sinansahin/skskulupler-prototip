b7ed9643e39cb02047a633e990ee2f2a5d55d5ea
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-08-27 13:44:03 +0300

    küçük style değişikleri

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,7 +4,7 @@ class Club < ActiveRecord::Base
 	has_many :activities
 	has_and_belongs_to_many :users
 
-	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "omu.jpg"
  	 validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
