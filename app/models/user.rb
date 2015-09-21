f2a117453f96ae96f5a0cf9b9bfbcd48f0d03672
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-09-21 08:02:50 +0300

    Admin panel config version 0.5 not finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :club_roles, dependent: :destroy
   has_many :clubs, through:  :club_roles
   
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "user.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :first_name, :last_name, :email, presence: true
   validates :password, presence: true, on: :create
