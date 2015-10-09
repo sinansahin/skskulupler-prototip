a65f4d3d981e55d6185a635ad9031087cc593e7e
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2015-10-09 12:30:38 +0300

    Bazı validation ve form ayarları.

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,10 +4,11 @@ class Club < ActiveRecord::Base
 	has_many :activities
 	#has_and_belongs_to_many :users
 	has_many :club_roles, dependent: :destroy
-  	has_many :users, through:  :club_role
+  	has_many :users, through: :club_role
 
-	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "omu.jpg"
- 	 validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
- 	 validates :name, :logo, :supervisor, :regulation, :description, :email, presence: true
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "omu.jpg"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+	validates :name, :avatar, :supervisor, :regulation, :description, presence: {message: "alanı boş bırakılamaz"}
+	validates :email, presence: { message: "alanı boş bırakılamaz."}, uniqueness: {message: "alanı benzersiz olmalıdır."}
 
 end
--- app/views/admin/clubs/index.json.jbuilder
+++ app/views/admin/clubs/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@clubs) do |club|
-  json.extract! club, :id, :name, :logo, :supervisor, :regulation, :description, :email
+  json.extract! club, :id, :name, :avatar, :supervisor, :regulation, :description, :email
   json.url club_url(club, format: :json)
 end
