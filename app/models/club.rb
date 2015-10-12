445e8690315fae03b847912c3551be211a71baa6
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2015-10-12 09:21:08 +0300

    Modellemede küçük ayarlar.

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 class Club < ActiveRecord::Base
 	#has_one :club_role, dependent: :destroy
-	has_many :notices
-	has_many :activities
+	has_many :notices, dependent: :destroy
+	has_many :activities, dependent: :destroy
 	#has_and_belongs_to_many :users
 	has_many :club_roles, dependent: :destroy
   	has_many :users, through: :club_role
