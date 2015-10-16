c00cad165370a13ad2cac9ad2d99318aefb68fc7
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2015-10-16 11:29:04 +0300

    Haberler Validation

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,4 +1,6 @@
 class Info < ActiveRecord::Base
+	  validates :title , :content , presence: {message: "alanı boş bırakılamaz"}
+	  validates :avatar, presence: {message: "yüklenmesi zorunludur."} , on: :create
 	  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   		validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
