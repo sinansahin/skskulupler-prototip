2799141f7ca8162784e1d05a7bb5b0f07ef6bd44
Emrullah Sağlam <emrullah.saglam@bil.omu.edu.tr>
2015-10-16 11:26:01 +0300

    Avatar Validation Düzeltme

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@
 class Activity < ActiveRecord::Base
 	belongs_to :club
 	validates :club_id, :title, :description, :start_date, :finish_date, :address, presence: {message: "alanı boş bırakılamaz"}
-	validates :avatar, presence: {message: "yüklenmesi zorunludur."}
+	validates :avatar, presence: {message: "yüklenmesi zorunludur."} , on: :create
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "omu.png"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
