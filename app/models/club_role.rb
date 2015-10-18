bcb5e6b090871472c78701a6c8927f46d4b95c42
cevdetakaydin <cevdet.akaydin@bil.omu.edu.tr>
2015-10-18 16:18:55 +0300

    heroku fix

--- app/models/club_role.rb
+++ app/models/club_role.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 class ClubRole < ActiveRecord::Base
-	belongs_to :role
 	belongs_to :user 
   	belongs_to :club
 end
--- db/migrate/20150827114951_rename_and_change_type_name_to_club_role.rb
+++ db/migrate/20150827114951_rename_and_change_type_name_to_club_role.rb
@@ -1,7 +1,5 @@
 class RenameAndChangeTypeNameToClubRole < ActiveRecord::Migration
 def change
-rename_column :club_roles, :name, :role_id
-change_column :club_roles, :role_id, :integer
 
 end
 end
